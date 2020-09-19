440bda45c773f817d633488bd9ec793983728d8e
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-09-20 00:17:05 +0500

    add: generated vue, svelte, ios app

---
+++ ios/todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Ruslan Usmanov on 20.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
