22204a0daf4f2ed5447c0b70df6402048d44db24
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-09-28 04:23:09 +0500

    add(react): add remaining time

--- react/src/ui/task-item/TaskItem.tsx
+++ react/src/ui/task-item/TaskItem.tsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom'
 import './TaskItem.scss'
 import { MoreMenu } from '../more-menu/MoreMenu'
 import { ReactComponent as AccessTimeIcon } from '../../assets/svg/access_time-24px.svg'
+import { ReactComponent as EastIcon } from '../../assets/svg/east-24px.svg'
 import { Task } from '../../models/task'
 
 type TaskItemProps = {
@@ -15,19 +16,32 @@ export const TaskItem = ({ task, onClick }: TaskItemProps) => {
   const [isActive, setIsActive] = useState(false)
   const [remaining, setRemaining] = useState('')
 
-  const onTime = (e: any) => {
-    e.stopPropagation()
+  const onTime = (event: any) => {
+    event.stopPropagation()
     setIsActive(true)
     setRemaining(`Remaining ${task.date}...`)
   }
 
+  const onBack = (event: any) => {
+    event.stopPropagation()
+    setIsActive(false)
+  }
+
   return (
     <div className="item" onClick={() => history.push('/task')}>
-      <div className="text">{task?.title}</div>
-      <div className="buttons">
-        <MoreMenu/>
-        <AccessTimeIcon onClick={onTime}/>
-      </div>
+      <div className="text">{isActive ? remaining : task?.title}</div>
+      {
+        isActive
+          ?
+          <div className="buttons">
+            <EastIcon onClick={onBack}/>
+          </div>
+          :
+          <div className="buttons">
+            <MoreMenu/>
+            <AccessTimeIcon onClick={onTime}/>
+          </div>
+      }
     </div>
   )
 }
