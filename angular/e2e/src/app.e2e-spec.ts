c1d0f8a33023f76923bb06a3f824c65d251867d4
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-09-28 17:48:19 +0500

    add(angular): e2e tests

--- angular/e2e/src/app.e2e-spec.ts
+++ angular/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, by, element, logging } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +8,33 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display app title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('todo-app app is running!');
+    expect(page.getTitleText()).toEqual('Delat\n\'');
+  });
+
+  it('should go to new task page', () => {
+    page.navigateTo();
+    element(by.tagName('input')).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/new');
+  });
+
+  it('should go to today tasks section settings page', () => {
+    page.navigateTo();
+    element(by.tagName('app-section-title')).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/section-settings?title=today');
+  });
+
+  it('should go to today task page', () => {
+    page.navigateTo();
+    element(by.tagName('app-task-item')).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/task?id=2');
+  });
+
+  it('should go to trashcan page', () => {
+    page.navigateTo();
+    element(by.css('.buttons > img:nth-child(1)')).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/trashcan');
   });
 
   afterEach(async () => {
