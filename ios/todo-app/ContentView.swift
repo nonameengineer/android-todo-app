26c56b0b166216900fa8fdbe6d98047235eb7a66
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-10-03 11:33:59 +0500

    add(iOS): title

--- ios/todo-app/ContentView.swift
+++ ios/todo-app/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack(spacing: 0.0) {
+                Text("Delat")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                    
+                Text("'")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                    .foregroundColor(Color.blue)
+            }
+        }
+        .padding(8.0)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 
