10198682c2bc56ca662249ca88a19f29c7ad8136
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-10-10 20:27:12 +0500

    add(android): example files

---
+++ android/app/src/main/java/com/example/todoapp/TodoApplication.kt
@@ -0,0 +1,22 @@
+package com.example.todoapp
+
+import android.app.Application
+import timber.log.Timber
+
+/**
+ * An application that lazily provides a repository. Note that this Service Locator pattern is
+ * used to simplify the sample. Consider a Dependency Injection framework.
+ *
+ * Also, sets up Timber in the DEBUG BuildConfig. Read Timber's documentation for production setups.
+ */
+class TodoApplication : Application() {
+
+    // Depends on the flavor,
+    val taskRepository: TasksRepository
+        get() = ServiceLocator.provideTasksRepository(this)
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
