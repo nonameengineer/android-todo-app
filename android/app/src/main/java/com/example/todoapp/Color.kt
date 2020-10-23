e5827946fd329464b314825388b8b5d03ac5909c
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-10-23 13:41:15 +0500

    Add Color enum

---
+++ android/app/src/main/java/com/example/todoapp/Color.kt
@@ -0,0 +1,12 @@
+package com.example.todoapp
+
+enum class Color(val rgb: Int) {
+    RED(0xFA114F),
+    ORANGE(0xFF3B30),
+    YELLOW(0xFFE620),
+    GREEN(0x06DE71),
+    CYAN(0x00F5E9),
+    BLUE(0x5AC8FA),
+    DARK_BLUE(0x2094FA),
+    PURPLE(0x787AFF)
+}
