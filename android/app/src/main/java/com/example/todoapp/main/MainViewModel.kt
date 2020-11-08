781e174019370182f47f934acbfd117ac3fe56a4
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-11-08 18:14:40 +0500

    Add TasksRepository

--- android/app/src/main/java/com/example/todoapp/main/MainViewModel.kt
+++ android/app/src/main/java/com/example/todoapp/main/MainViewModel.kt
@@ -1,7 +1,146 @@
 package com.example.todoapp.main
 
-import androidx.lifecycle.ViewModel
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.lifecycle.*
+import com.example.todoapp.Event
+import com.example.todoapp.data.Result
+import com.example.todoapp.data.Task
+import com.example.todoapp.data.source.TasksRepository
+import com.example.todoapp.util.wrapEspressoIdlingResource
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val tasksRepository: TasksRepository
+) : ViewModel() {
+
+    private val _items = MutableLiveData<List<Task>>().apply { value = emptyList() }
+    val items: LiveData<List<Task>> = _items
+
+    private val _dataLoading = MutableLiveData<Boolean>()
+    val dataLoading: LiveData<Boolean> = _dataLoading
+
+    private val _currentFilteringLabel = MutableLiveData<Int>()
+    val currentFilteringLabel: LiveData<Int> = _currentFilteringLabel
+
+    private val _noTasksLabel = MutableLiveData<Int>()
+    val noTasksLabel: LiveData<Int> = _noTasksLabel
+
+    private val _noTaskIconRes = MutableLiveData<Int>()
+    val noTaskIconRes: LiveData<Int> = _noTaskIconRes
+
+    private val _tasksAddViewVisible = MutableLiveData<Boolean>()
+    val tasksAddViewVisible: LiveData<Boolean> = _tasksAddViewVisible
+
+    private val _snackbarText = MutableLiveData<Event<Int>>()
+    val snackbarMessage: LiveData<Event<Int>> = _snackbarText
+
+    private var _currentFiltering = "ALL TASKS"
+
+    // Not used at the moment
+    private val isDataLoadingError = MutableLiveData<Boolean>()
+
+    private val _openTaskEvent = MutableLiveData<Event<String>>()
+    val openTaskEvent: LiveData<Event<String>> = _openTaskEvent
+
+    private val _newTaskEvent = MutableLiveData<Event<Unit>>()
+    val newTaskEvent: LiveData<Event<Unit>> = _newTaskEvent
+
+    // This LiveData depends on another so we can use a transformation.
+    val empty: LiveData<Boolean> = Transformations.map(_items) {
+        it.isEmpty()
+    }
+
+    init {
+        // Set initial state
+        setFiltering("")
+        loadTasks(true)
+    }
+
+    /**
+     * Sets the current task filtering type.
+     *
+     * @param requestType Can be [TasksFilterType.ALL_TASKS],
+     * [TasksFilterType.COMPLETED_TASKS], or
+     * [TasksFilterType.ACTIVE_TASKS]
+     */
+    fun setFiltering(requestType: String) {
+        _currentFiltering = requestType
+    }
+
+    private fun setFilter(
+        @StringRes filteringLabelString: Int, @StringRes noTasksLabelString: Int,
+        @DrawableRes noTaskIconDrawable: Int, tasksAddVisible: Boolean
+    ) {
+        _currentFilteringLabel.value = filteringLabelString
+        _noTasksLabel.value = noTasksLabelString
+        _noTaskIconRes.value = noTaskIconDrawable
+        _tasksAddViewVisible.value = tasksAddVisible
+    }
+
+    fun clearCompletedTasks() {
+        viewModelScope.launch {
+            // Refresh list to show the new state
+            loadTasks(false)
+        }
+    }
+
+    fun completeTask(task: Task, completed: Boolean) = viewModelScope.launch {
+        // Refresh list to show the new state
+        loadTasks(false)
+    }
+
+    /**
+     * Called by the Data Binding library and the FAB's click listener.
+     */
+    fun addNewTask() {
+        _newTaskEvent.value = Event(Unit)
+    }
+
+    /**
+     * Called by Data Binding.
+     */
+    fun openTask(taskId: String) {
+        _openTaskEvent.value = Event(taskId)
+    }
+
+    fun showEditResultMessage(result: Int) {
+
+    }
+
+    private fun showSnackbarMessage(message: Int) {
+        _snackbarText.value = Event(message)
+    }
+
+    /**
+     * @param forceUpdate   Pass in true to refresh the data in the [TasksDataSource]
+     */
+    fun loadTasks(forceUpdate: Boolean) {
+        _dataLoading.value = true
+
+        wrapEspressoIdlingResource {
+
+            viewModelScope.launch {
+                val tasksResult = tasksRepository.getTasks(forceUpdate)
+
+                if (tasksResult is Result.Success) {
+                    val tasks = tasksResult.data
+
+                    val tasksToShow = java.util.ArrayList<Task>()
+                    isDataLoadingError.value = false
+                    _items.value = ArrayList(tasksToShow)
+                } else {
+                    isDataLoadingError.value = false
+                    _items.value = emptyList()
+                }
+
+                _dataLoading.value = false
+            }
+        }
+    }
+
+    fun refresh() {
+        loadTasks(true)
+    }
 }
